4a9eb4da324eb0c45c639e5df13cc8c0a401d3f6
lapo <lapo.bartolacci97@gmail.com>
2020-01-05 21:02:04 +0100

    Updated confusion matrix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score, confusion_matrix
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 FEATURE_TRAIN_PATH = "UCI HAR Dataset/train/X_train.txt"
 TARGET_TRAIN_PATH = "UCI HAR Dataset/train/y_train.txt"
@@ -33,18 +32,12 @@ target_pred = ppn.predict(features_test_std)
 labels=np.loadtxt(fname="UCI HAR Dataset/activity_labels.txt", dtype=str, usecols=(1))
 cm= confusion_matrix(target_test, target_pred)
 df = pd.DataFrame(cm, columns=labels, index=labels)
-sum_matrix = df.to_numpy().sum()
 df.loc['Precision %',:]= df.sum(axis=0)
 df.loc[:,'Recall %'] = df.sum(axis=1)
-sum_diag = 0
+
 for label in labels:
     df.at["Precision %",label] = df.at[label,label] / df.at["Precision %",label] * 100
     df.at[label, "Recall %"] = df.at[label, label] / df.at[label, "Recall %"] * 100
-    sum_diag = sum_diag + df.at[label, label]
-df.at["Precision %", "Recall %"] = sum_diag / sum_matrix * 100
+
+df.at["Precision %", "Recall %"] = accuracy_score(target_test, target_pred) * 100
 print(df)
-
-
-# View model accuracy
-# Defined as (1.0 - (# wrong predictions / # total observations))
-print('Accuracy: %.2f' % accuracy_score(target_test, target_pred))
