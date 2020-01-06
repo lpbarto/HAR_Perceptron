fde35dad3d53e3137c7999c59fc5078b20205538
lapo <lapo.bartolacci97@gmail.com>
2020-01-06 19:30:10 +0100

    matrix format and csv export

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score, confusion_matrix
 import numpy as np
 import pandas as pd
+from mlxtend.plotting import plot_decision_regions
+import matplotlib.pyplot as plt
 
 FEATURE_TRAIN_PATH = "UCI HAR Dataset/train/X_train.txt"
 TARGET_TRAIN_PATH = "UCI HAR Dataset/train/y_train.txt"
 FEATURE_TEST_PATH = "UCI HAR Dataset/test/X_test.txt"
 TARGET_TEST_PATH = "UCI HAR Dataset/test/y_test.txt"
-
 
 features_train = np.loadtxt(fname=FEATURE_TRAIN_PATH)
 target_train = np.loadtxt(fname=TARGET_TRAIN_PATH)
@@ -29,15 +30,33 @@ ppn.fit(features_train_std, target_train)
 # Apply the trained perceptron on the features data to make predicts for the target test data
 target_pred = ppn.predict(features_test_std)
 
-labels=np.loadtxt(fname="UCI HAR Dataset/activity_labels.txt", dtype=str, usecols=(1))
-cm= confusion_matrix(target_test, target_pred)
+labels = np.loadtxt(fname="UCI HAR Dataset/activity_labels.txt", dtype=str, usecols=(1))
+cm = confusion_matrix(target_test, target_pred)
 df = pd.DataFrame(cm, columns=labels, index=labels)
-df.loc['Precision %',:]= df.sum(axis=0)
-df.loc[:,'Recall %'] = df.sum(axis=1)
+df.loc['Precision %', :] = df.sum(axis=0)
+df.loc[:, 'Recall %'] = df.sum(axis=1)
 
 for label in labels:
-    df.at["Precision %",label] = df.at[label,label] / df.at["Precision %",label] * 100
-    df.at[label, "Recall %"] = df.at[label, label] / df.at[label, "Recall %"] * 100
+    df.at["Precision %", label] = (df.at[label, label] / df.at["Precision %", label] * 100).astype(float).round(2)
+    df.at[label, "Recall %"] = (df.at[label, label] / df.at[label, "Recall %"] * 100).astype(float).round(2)
 
-df.at["Precision %", "Recall %"] = accuracy_score(target_test, target_pred) * 100
-print(df)
+df.at["Precision %", "Recall %"] = (accuracy_score(target_test, target_pred) * 100).round(2)
+
+
+df.style.set_table_styles(
+    [dict(selector="th", props=[('max-width', '80px')]),
+     dict(selector="th.col_heading",
+          props=[("writing-mode", "vertical-rl"),
+                 ('transform', 'rotateZ(-90deg)'),
+                 ])]
+)
+pd.options.display.width = 0
+df.to_csv("confusion_matrix.csv")
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(df)
+
+
+# Plotting decision regions
+# plot_decision_regions(features_test, target_test.astype(np.integer), clf=ppn, legend=2)
+# Adding axes annotations
+# plt.show()
